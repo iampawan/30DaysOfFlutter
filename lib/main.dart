b362120baeb6cccb9064584f22bbb1cf9a8ef4b7
iampawan <mtechviral@gmail.com>
2021-02-02 16:42:18 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("Welcome to 30 days of flutter"),
+          ),
+        ),
+      ),
+    );
+  }
+}
